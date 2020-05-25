e02a3e7ae902cb71938bf2b61e0e2f35c1b8920a
mavidelaru <vidalmarta11@gmail.com>
2020-05-25 17:53:07 +0200

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import usersFormAnswers from '../app/services/data.json';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'covid-crealogix';
+  // usersFormAnswers: any = usersFormAnswers;
 }
--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { interval, timer, from, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { async } from '@angular/core/testing';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-result',
@@ -12,12 +14,22 @@ export class ResultComponent implements OnInit {
   obs: any;
   newArray = [1,2,3,4,5,6,7,8,9,20];
 
-  searches;
-  
+  searchField: FormControl;
+
+  searches: string[] = [];
 
   constructor() {}
 
-  public ngOnInit(): void {
+   ngOnInit() {
+
+    this.searchField = new FormControl();
+    this.searchField.valueChanges
+      .pipe(debounceTime(400))
+      .pipe(distinctUntilChanged())
+      .subscribe(term => {
+        this.searches.push(term);
+      })
+
     // const contador = interval(1000);
 
     // const takeFourNumbers = contador.pipe(take(4));
